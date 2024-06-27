b4ae95ac9c1e3fa3c4b22ebc0275b62ce7e03df4
PAULO HENRIQUE DOS SANTOS <paulo.hsantos26@senacsp.edu.br>
2024-06-27 12:01:31 -0300

    resolução atividades

---
+++ aula02/at1.js
@@ -0,0 +1,13 @@
+// Escreva um programa que verifique se um número fornecido pelo usuário é positivo, negativo ou zero.
+
+numeroUsuario = 0
+
+if (numeroUsuario > 0) {
+    console.log('O numero digitado é POSITIVO');
+
+} else if (numeroUsuario < 0) {
+    console.log('O numero digitado é NEGATIVO');
+
+} else if (numeroUsuario == 0){
+    console.log('O numero digitado é IGUAL que 0');
+}
---
+++ aula02/at13.js
@@ -0,0 +1,13 @@
+numeroUsuario = 0
+
+numeroSorteado = 54
+
+if (numeroUsuario > numeroSorteado) {
+    console.log('O numero digitado é Maior');
+
+} else if (numeroUsuario < numeroSorteado) {
+    console.log('O numero digitado é Menor');
+
+} else if (numeroUsuario == numeroSorteado){
+    console.log('O numero digitado é IGUAL ao SORTEADO...');
+}
