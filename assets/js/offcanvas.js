84c110f54c27a1ce7a00d55cd3544740790ca0e5
paulo santos <paulo.whsantos@hotmail.com>
2024-11-21 08:27:14 -0300

    menu hamburguer

---
+++ assets/js/offcanvas.js
@@ -0,0 +1,9 @@
+function abrirNav(){
+    const menu = document.getElementById("offcanvas")
+    menu.style.width = "250px"
+}
+
+function fecharNav(){
+    const menu = document.getElementById("offcanvas")
+    menu.style.width = "0"
+}
