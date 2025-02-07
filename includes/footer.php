69771e8250f850e174d9e66063e59ffd5ce92c81
Paulo Henrique <ph744915@gmail.com>
2025-02-07 08:26:53 -0300

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,49 @@
+<footer class="footer">
+    <main class="container">
+        <div class="row">
+
+            <div class="col-md-3">
+                <h5 class="font-weight-bold">MENU</h5>
+                <ul class="list-unstyled mt-3">
+                    <li><a href="./index.php">Inicio</a></li>
+                    <li><a href="./index.php#filmes">Filmes</a></li>
+                    <li><a href="./sobre.php">Sobre</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3">
+                <h5 class="font-weight-bold">Sobre a Gente</h5>
+                <ul class="list-unstyled  mt-3">
+                    <li><a href="./sobre.php">Sobre</a></li>
+                    <li><a href="./contato.php">Contato</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3">
+                <h5 class="font-weight-bold">Descubra Mais</h5>
+
+                <ul class="col-md-6 list-unstyled  mt-3">
+                    <li><a target="_blank" href="#">projeto 1</a></li>
+                    <li><a target="_blank" href="#">projeto 1</a></li>
+                    <li><a target="_blank" href="#">projeto 1</a></li>
+                </ul>
+
+            </div>
+            <div class="col-md-3">
+                <h5 class="font-weight-bold">Descubra Mais</h5>
+                <ul class="col-md-6 list-unstyled  mt-3">
+                    <li><a target="_blank" href="#">projeto 1</a></li>
+                    <li><a target="_blank" href="#">projeto 1</a></li>
+                    <li><a target="_blank" href="#">projeto 1</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class=" footer-bottom">
+            <p>&copy; 2025. Este site foi desenvolvido com a turma de TI24. Não reservamos os direitos e os valores
+                apresentados são meramente ilustrativos para fins didáticos.</p>
+        </div>
+    </main>
+</footer>
 </body>
 <script src="./assets/js/carrossel.js"></script>
 <script src="./assets/js/offcanvas.js"></script>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 $titulo = 'CineBox - Início';
-include './includes/header.php' ?>
+include './includes/header.php';
+?>
 
 <section id="banner">
     <main class="carrossel-container">
@@ -23,27 +24,29 @@ include './includes/header.php' ?>
     <main class="container">
         <div class="row">
 
-        <?php for ($i = 0; $i < 8; $i++) { ?>
-            <div class="col col-3">
-                <figure>
-                    <img src="./assets/img/poster/avatar-o-caminho-da-agua.png" alt="Poster Avatar o caminho da água"
-                        class="foto-produto">
-                    <figcaption>
-                        <h4>Avatar o Caminho da Água</h4>
-                        <span class="preco">R$ 15,90</span>
-                        <p class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ipsa rerum
-                            dolore voluptates labore deserunt iste. Voluptatem provident necessitatibus nemo tempore
-                            quia, deserunt quo ad illo. Eum ut quia necessitatibus!</p>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                        <label style="background-color: #D966BA;">Ficção</label>
-                    </span>
-                </figure>
-            </div>
-        <?php } ?>
+            <?php for ($i = 0; $i < 8; $i++) { ?>
+                <div class="col col-3">
+                    <figure>
+                        <img src="./assets/img/poster/avatar-o-caminho-da-agua.png" alt="Poster Avatar o caminho da água"
+                            class="foto-produto">
+                        <figcaption>
+                            <h4>Avatar o Caminho da Água</h4>
+                            <span class="preco">R$ 15,90</span>
+                            <p class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ipsa rerum
+                                dolore voluptates labore deserunt iste. Voluptatem provident necessitatibus nemo tempore
+                                quia, deserunt quo ad illo. Eum ut quia necessitatibus!</p>
+                        </figcaption>
+                        <span class="genero">
+                            <label style="background-color: #070373;">Fantasia</label>
+                            <label style="background-color: #580259;">Drama</label>
+                            <label style="background-color: #D966BA;">Ficção</label>
+                        </span>
+                    </figure>
+                </div>
+            <?php } ?>
 
         </div>
     </main>
 </section>
+
+<?php include './includes/footer.php'  ?>
