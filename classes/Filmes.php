79ef480592400bdbdd0e8c63ecd9af1df644559c
Paulo Henrique <ph744915@gmail.com>
2025-02-07 11:27:05 -0300

    class do Filme

---
+++ classes/Filmes.php
@@ -0,0 +1,22 @@
+<?php
+
+class Filmes
+{
+    function listarFilmesBanco()
+    {
+        // codigos...
+        // codigos...
+        // codigos...
+
+        return 'Aqui eu tenho um retorno boladão';
+    }
+
+    function outraFunction()
+    {
+        // codigos...
+        // codigos...
+        // codigos...
+        
+        return 'Aqui eu tenho um retorno boladão';
+    }
+}
--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
+require './classes/Filmes.php';
+
 $titulo = 'CineBox - Início';
 include './includes/header.php';
 include './includes/banner.php';
+
+$filmes = new Filmes();
+$dados = $filmes->listarFilmesBanco();
+
+echo $dados;
+
 
 include './includes/filme_lista.php';
 
