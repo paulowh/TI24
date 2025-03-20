c5c41c0e6a49018cec55594a4ffc9b4db1ed6b31
Paulo Henrique <ph744915@gmail.com>
2025-03-20 08:11:47 -0300

    sistema de login

--- login/auxlogin.php
+++ login/auxlogin.php
@@ -16,6 +16,48 @@ $consultaUsuarioSenha = 'SELECT * FROM tb_usuario WHERE usuario = "' . $userForm
 
 $resultado = $banco->query($consultaUsuarioSenha)->fetch();
 
+$status = $resultado['status'];
+
+$_
+?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+<?php if ($status == 'admin') { ?>
+
+<h1>Bem Vindo USUARIO ADMIN</h1>
+
+<?php } ?>
+
+
+<h1>Bem Vindo USUARIO COMUM</h1>
+
+<a href="#" class="btn btn-success" >cadastrar -comum</a>
+<a href="#" class="btn btn-primary" >Abrir -comum</a> 
+
+
+<a href="#" class="btn btn-warning <?= $status != 'admin' ? 'disabled' : ''  ?>" >Editar -admin</a> 
+<a href="#" class="btn btn-danger"  >Excluir -admin</a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+die;
 if (!empty($resultado) && $resultado != false) {
     header('location:loginSucesso.php');
 } else {
--- login/loginSucesso.php
+++ login/loginSucesso.php
@@ -1,1 +1,24 @@
-<h1>Bem Vindo</h1>
+
+<?php
+    $dados = [
+        'nome' => '',
+        'telefone' => '199926323292',
+        'email' => 'paulo@gmail.com',
+        'nascimento' => '2002'
+    ]
+?>
+
+
+<?= isset($dados) && empty($dados) 
+    ?'<form action="cadastrar.php" method="get">' 
+    :'<form action="editar.php" method="get">' ?>
+
+
+    <input type="text" placeholder="nome" value="<?= !empty($dados['nome']) ? $dados['nome'] : '' ?>">
+    <input type="text" placeholder="telefone">
+    <input type="text" placeholder="email">
+    <input type="text" placeholder="nascimento">
+
+    <input type="submit">
+
+</form>
