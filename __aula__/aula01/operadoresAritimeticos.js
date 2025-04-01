335b3b6990017319354c9d35f3d78dda7a08d143
Paulo Henrique <ph744915@gmail.com>
2025-04-01 09:40:18 -0300

    org __aula__

---
+++ __aula__/aula01/operadoresAritimeticos.js
@@ -0,0 +1,50 @@
+// String
+// Boolean
+// Int
+// Float
+
+var numero1 = 10
+var numero2 = 14
+
+var soma = numero1 + numero2
+var subtracao = numero1 - numero2
+var multiplicacao = numero1 * numero2
+var divisao = numero1 / numero2
+
+
+console.log("Soma dos valores:", soma)
+console.log("Subtração dos valores:", subtracao)
+console.log("Multiplicação dos valores:", multiplicacao)
+console.log("Divisão dos valores:", divisao)
+
+/**
+ * Atividade 01
+ * Imprimir a tabuada de um numero a sua vontade....
+ * 
+ * Guarde o valor de um numero em uma variavel
+ * Agora calcule a tabuada em cima desse valor ex: 
+ * 
+ * eu escolhi o numero 3
+ * 
+ * resultado esperado:
+ * 1 * 3 = 3
+ * 2 * 3 = 6
+ * 3 * 3 = 9
+ * e por ai vai....
+ * 
+ */
+
+console.log("===================================")
+
+var numeroEscolhido = 6
+
+console.log(numeroEscolhido, "x 1 = ", numeroEscolhido * 1)
+console.log(numeroEscolhido, "x 2 = ", numeroEscolhido * 2)
+console.log(numeroEscolhido, "x 3 = ", numeroEscolhido * 3)
+console.log(numeroEscolhido, "x 4 = ", numeroEscolhido * 4)
+console.log(numeroEscolhido, "x 5 = ", numeroEscolhido * 5)
+console.log(numeroEscolhido, "x 6 = ", numeroEscolhido * 6)
+console.log(numeroEscolhido, "x 7 = ", numeroEscolhido * 7)
+console.log(numeroEscolhido, "x 8 = ", numeroEscolhido * 8)
+console.log(numeroEscolhido, "x 9 = ", numeroEscolhido * 9)
+console.log(numeroEscolhido, "x 10 = ", numeroEscolhido * 10)
